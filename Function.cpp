8300ce2f5b5c2e80ab3ee879fbacf3e87e87d609
Elliot <elliotn46@gmail.com>
2019-05-09 16:54:02 -0700

    functions getting better. works on its own

---
+++ Function.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Elliot on 5/9/2019.
+//
+
+#include "Function.h"
---
+++ Function.h
@@ -0,0 +1,14 @@
+//
+// Created by Elliot on 5/9/2019.
+//
+
+#ifndef EXPRINTER_FUNCTION_H
+#define EXPRINTER_FUNCTION_H
+
+
+class Function {
+
+};
+
+
+#endif //EXPRINTER_FUNCTION_H
